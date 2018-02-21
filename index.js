8bdce7a22bb0318e3b308e167c52a7269a3b6c69
tennisonchan <tennison.chan+gh@gmail.com>
2018-02-21 19:04:47 +0800

    Flattern the process using async and await

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const commandLineArgs = require('command-line-args');
-const getUsage = require('command-line-usage')
+const getUsage = require('command-line-usage');
 const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
 const path = require('path');
-const {optionDefinitions, usageDefinitions} = require('./option-definitions');
+const { optionDefinitions, usageDefinitions } = require('./option-definitions');
 
 const options = commandLineArgs(optionDefinitions);
 
@@ -23,42 +23,129 @@ console.log('Output format: ', outputFormat);
 console.log('Output size: ', outputSize);
 console.log('Threshold: ', clipLengthThreshold);
 
-fs.readdir(dirPath, function (err, files) {
-  if (err) {
-    console.error("Could not list the directory.", err);
-    process.exit(1);
-  }
+function temp() {
+  fs.readdir(dirPath, function(err, files) {
+    if (err) {
+      console.error('Could not list the directory.', err);
+      process.exit(1);
+    }
 
-  files.forEach(function (file, index) {
-    let filePath = path.join(dirPath, file);
-    let extname = path.extname(filePath);
-    let dirName = path.dirname(filePath).split(path.sep).pop();
+    files.forEach(function(file, index) {
+      if (!allowFileTypes.includes(path.extname(file))) return false;
 
-    if (!allowFileTypes.includes(extname)) return false;
+      let filePath = path.join(dirPath, file);
+      let dirName = path
+        .dirname(filePath)
+        .split(path.sep)
+        .pop();
 
-    ffmpeg.ffprobe(filePath, function(err, metadata) {
-      let { duration, tags: { creation_time } } = metadata.format;
-      let filenamePrefix = `${dirName}-${+new Date(creation_time)}`;
-      duration = Number(duration);
+      ffmpeg.ffprobe(filePath, function(err, metadata) {
+        let { duration, tags: { creation_time } } = metadata.format;
+        let filenamePrefix = `${dirName}-${+new Date(creation_time)}`;
+        duration = Number(duration);
 
-      let count = Math.ceil(duration / clipLengthThreshold);
-      let eachDuration = duration / count;
+        let count = Math.ceil(duration / clipLengthThreshold);
+        let eachDuration = duration / count;
 
-      for(let i = 0; i < count; i++) {
-        let suffix = count === 1 ? '' : `-${i+1}`;
-        let outputFilename = `${filenamePrefix}${suffix}${outputFormat}`;
+        for (let i = 0; i < count; i++) {
+          let suffix = count === 1 ? '' : `-${i + 1}`;
+          let outputFilename = `${filenamePrefix}${suffix}${outputFormat}`;
+          let seekInput = eachDuration * i;
+          let outputPath = path.join(dirPath, outputFilename);
 
-        ffmpeg(filePath)
-          .size(outputSize)
-          .seekInput(eachDuration * i)
-          .duration(eachDuration)
-          .save(path.join(dirPath, outputFilename))
-          .on('error', err => console.error(err))
-          .on('end', () => {
-            let { duration, size } = arguments[1].format;
-            console.log(`${(size / 1024 / 1024).toFixed(1)}M ${Math.ceil(duration)}s ${outputFilename}`)
-          });
-      }
+          ffmpeg(filePath)
+            .size(outputSize)
+            .seekInput(seekInput)
+            .duration(eachDuration)
+            .save(outputPath)
+            .on('error', err => console.error(err))
+            .on('end', () => {
+              console.log(`done: ${outputFilename}`);
+            });
+        }
+      });
     });
   });
-});
+}
+
+async function main() {
+  const list = await readFiles(dirPath);
+  let i = 0;
+
+  (function next() {
+    let videoInfo = list[i++];
+    if (videoInfo) {
+      console.log('Video Info', videoInfo);
+      cutVideo(videoInfo, next);
+    }
+  })();
+}
+
+main();
+
+function readFiles(dirPath) {
+  return new Promise((res, rej) => {
+    let j = 0;
+    const list = [];
+    fs.readdir(dirPath, function(err, files) {
+      if (err) {
+        console.error('Could not list the directory.', err);
+        process.exit(1);
+      }
+      (function next() {
+        let file = files[j++];
+        if (!file) {
+          res(list);
+          return false;
+        }
+        if (!allowFileTypes.includes(path.extname(file))) {
+          next();
+          return false;
+        }
+
+        let filePath = path.join(dirPath, file);
+        let dirName = path
+          .dirname(filePath)
+          .split(path.sep)
+          .pop();
+
+        ffmpeg.ffprobe(filePath, function(err, metadata) {
+          if (err) {
+            console.error(err);
+            process.exit(1);
+          }
+
+          let { duration, tags: { creation_time } } = metadata.format;
+          let filenamePrefix = `${dirName}-${+new Date(creation_time)}`;
+          duration = Number(duration);
+
+          let count = Math.ceil(duration / clipLengthThreshold);
+          let eachDuration = Math.ceil(duration / count);
+
+          for (let i = 0; i < count; i++) {
+            let suffix = count === 1 ? '' : `-${i + 1}`;
+            let outputFilename = `${filenamePrefix}${suffix}${outputFormat}`;
+            let seekInput = eachDuration * i;
+            let outputPath = path.join(dirPath, outputFilename);
+
+            list.push({ filePath, outputSize, seekInput, eachDuration, outputPath, outputFilename });
+          }
+          next();
+        });
+      })();
+    });
+  });
+}
+
+function cutVideo({ filePath, outputSize, seekInput, eachDuration, outputPath, outputFilename }, next) {
+  ffmpeg(filePath)
+    .size(outputSize)
+    .seekInput(seekInput)
+    .duration(eachDuration)
+    .save(outputPath)
+    .on('error', err => console.error(err))
+    .on('end', () => {
+      console.log(`done: ${outputFilename}`);
+      next();
+    });
+}
